661bda164a738fd87a282c587eeb6160597d6dd2
Akhil Boddu <akhil1234mara@gmail.com>
2020-11-05 10:01:58 +0300

    v2

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Version 2 stuff
         </a>
       </header>
     </div>
